62ff13eba89d36ec8149695dddf78aa72b1295c6
Paulo Luan <paulonfsu2@hotmail.com>
2013-01-14 12:56:21 -0200

    add pt-br language to i18n

---
+++ geoexplorer/app/static/script/app/locale/app/pt-br.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2009-2010 The Open Planning Project
+ */
+
+GeoExt.Lang.add("pt-br", {
+    "GeoExplorer.prototype": {
+        zoomSliderText: "<div>Nivel de detalhe: {zoom}</div><div>Escala: 1:{scale}</div>",
+        loadConfigErrorText: "Problemas na leitura da última configuração salva: <br />",
+        loadConfigErrorDefaultText: "Erro no servidor.",
+        xhrTroubleText: "Problemas de comunicação: Estado ",
+        layersText: "Camadas",
+        titleText: "Título",
+        bookmarkText: "URL do marcador",
+        permakinkText: "Link permanente",
+        appInfoText: "GeoExplorer",
+        aboutText: "sobre o Geoexplorer",
+        mapInfoText: "Informação do mapa",
+        descriptionText: "Descrição",
+        contactText: "Contato",
+        aboutThisMapText: "Sobre este mapa"
+    },
+    "GeoExplorer.Composer.prototype": {
+        mapText: "Mapa",
+        tableText: "Tabela",
+        queryText: "Query",
+        exportMapText: "Exportar Mapa",
+        saveMapText: "Salvar Mapa",
+        saveErrorText: "Problemas ao Salvar: ",
+        toolsTitle: "Escolha os elementos que deseja incluir em sua barra de ferramentas:",
+        previewText: "Visualizar prévia",
+        backText: "Anterior",
+        nextText: "Seguinte",
+        loginText: "Login",
+        loginErrorText: "Usuário ou senha inválidos.",
+        userFieldText: "Uuário",
+        passwordFieldText: "Senha",
+        logoutConfirmTitle: "Atenção",
+        logoutConfirmMessage: "Sair irá desfazer quaisquer alterações não salvas, remova quaisquer camadas que você pode ter adicionado, e redefina a composição do mapa. Você deseja salvar sua composição antes de sair?"
+    }
+});
