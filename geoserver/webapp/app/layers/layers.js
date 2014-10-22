b99d4a987eb8fc1359e6caaccd25e2fd970abf72
Robert Denis <rdenis@boundlessgeo.com>
2014-10-22 16:20:31 -0700

    Altering the download menu to use ui.bootstrap.popover instead of custom functions. Removing extra classes from app.less and extra functions from layers.js

--- geoserver/webapp/app/layers/layers.js
+++ geoserver/webapp/app/layers/layers.js
@@ -242,28 +242,12 @@ angular.module('gsApp.layers', [
         });
       };
 
-      angular.element($window).bind('scroll', function(){
-        if ($scope.dropdownBoxSelected != '') {
-          $scope.setTop($scope.dropdownBoxSelected);
-        }
-      });
-
-      $scope.setTop = function(e) {
-        var winScroll = angular.element($window).scrollTop();
-        var icon = angular.element('#icon-' + e);
-        var iconTop = $window.Number(icon.css('top').replace('px',''));
-        var dropdownBox = angular.element('#download-' + e);
-        var dropdownBoxTop = iconTop - winScroll;
-
-        dropdownBox.css('top', dropdownBoxTop + 'px');
-        $scope.dropdownBoxSelected = e;
-      };
-
       $scope.pagingOptions = {
-        pageSizes: [25, 50, 100],
-        pageSize: 25,
+        pageSizes: [10, 50, 100],
+        pageSize: 10,
         currentPage: 1
       };
+
       $scope.filterOptions = {
           filterText: '',
           useExternalFilter: true
@@ -364,20 +348,14 @@ angular.module('gsApp.layers', [
             cellClass: 'text-center',
             sortable: false,
             cellTemplate:
-              '<li class="list-unstyled dropdown">' +
-                '<div id="icon-{{row.entity.name}}" class="icon-fixed"></div>' +
-                '<a ng-click="setTop(row.entity.name)" ' +
-                  'class="dropdown-toggle">' +
-                  '<div class="fa fa-download grid-icons" ' +
-                    'alt="Download Layer" title="Download Layer"></div>' +
-                '</a>' +
-                '<ul id="download-{{row.entity.name}}" ' +
-                  'class="download-dropdown dropdown-menu">' +
-                  '<li><a href="#">Shapefile</a></li>' +
-                  '<li><a href="#">GeoJSON</a></li>' +
-                  '<li><a href="#">KML</a></li>' +
-                '</ul>' +
-              '</li>',
+              '<a popover-placement="bottom" popover-html-unsafe="' +
+                '<a href=\'#\'>Shapefile</a><br />' +
+                '<a href=\'#\'>GeoJSON</a><br />' +
+                '<a href=\'#\'>KML</a>"' +
+                'popover-append-to-body="true">' +
+                '<div class="fa fa-download grid-icons" ' +
+                  'alt="Download Layer" title="Download Layer"></div>' +
+              '</a>',
             width: '7%'
           },
           {field: 'lastEdited',
