0634936b1988a83f7b46a3fff2a5a18a1b93d093
RobertDenis <robdenis@shaw.ca>
2015-02-03 09:30:45 -0800

    Adding graceful logout.

--- geoserver/webapp/app/app.js
+++ geoserver/webapp/app/app.js
@@ -19,8 +19,11 @@ angular.module('gsApp', [
   'gsApp.maps'
 ])
 .controller('AppCtrl', ['$scope', '$state', 'AppEvent', 'AppSession', '$window',
-    function($scope, $state, AppEvent, AppSession, $window) {
+    '$timeout', '$rootScope', '$modal', '$location', '$interval',
+    function($scope, $state, AppEvent, AppSession, $window, $timeout,
+      $rootScope, $modal, $location, $interval) {
       $scope.session = AppSession;
+      $rootScope.modal = false;
 
       var height = $window.innerHeight - 65;
       $scope.pageHeight = {'height': height};
@@ -41,14 +44,72 @@ angular.module('gsApp', [
         AppSession.clear();
         $state.go('login');
       });
+      $scope.$on(AppEvent.Timeout, function(e){
+        if (!$rootScope.timeoutModal) {
+          var modalInstance = $modal.open({
+            templateUrl: '/core/modals/timeout.tpl.html',
+            scope: $scope,
+            controller: ['$scope', '$window', '$modalInstance', '$state',
+              '$document',
+              function($scope, $window, $modalInstance, $state, $document) {
+                $scope.countdownTotal = 120;
+                $scope.countdown = $scope.countdownTotal;
+                $rootScope.timeoutModal = true;
+
+                $interval(function(){
+                  $scope.countdown--;
+                  $scope.bar = ($scope.countdown/$scope.countdownTotal) * 100;
+
+                  if ($scope.countdown == 0) {
+                    $rootScope.modalInstance.dismiss('cancel');
+                    AppSession.clear();
+                    $state.go('login');
+                  }
+                }, 1000);
+
+                $scope.cancel = function() {
+                  $scope.sessionTracker();
+                  $rootScope.timeoutModal = false;
+                  $modalInstance.dismiss('cancel');
+                };
+              }],
+            backdrop: 'static',
+            size: 'med'
+          });
+          $rootScope.modalInstance = modalInstance;
+        }
+      });
 
       // track app state changes
       $scope.state = {};
+      $scope.sessionTracker = function(){
+        if($location.path() != '/login') {
+          AppSession.update(AppSession.id, AppSession.user);
+
+          //$rootScope.sessionWarning = (AppSession.timeout * 1000) - 120000;
+          //$rootScope.sessionTimeout = AppSession.timeout * 1000;
+          //session timeout isn't coming back just yet.
+          $rootScope.sessionWarning = (1800 * 1000) - 120000;
+          $rootScope.sessionTimeout = 1800 * 1000;
+
+          $timeout(function(){}, $rootScope.sessionWarning).then(function() {
+            $scope.$broadcast(AppEvent.SessionTimeout);
+          });
+        }
+        else {
+          if ($rootScope.timeoutModal) {
+            $rootScope.timeoutModal = false;
+            $rootScope.modalInstance.dismiss('cancel');
+          }
+        }
+      };
+
       $scope.$on('$stateChangeSuccess',
-          function(e, to, toParams, from, fromParams) {
-              $scope.state.curr = {name: to, params: toParams};
-              $scope.state.prev = {name: from, params: fromParams};
-            });
+        function(e, to, toParams, from, fromParams) {
+          $scope.sessionTracker();
+          $scope.state.curr = {name: to, params: toParams};
+          $scope.state.prev = {name: from, params: fromParams};
+        });
     }])
 .factory('_', ['lodash',
     function(lodash) {
